1ea431761f48682b63c575c4f46a922e10507790
br-nikita <99282886+br-nikita@users.noreply.github.com>
2022-05-03 23:00:12 +0700

    Delete Brosalin/Code directory

--- Brosalin/Code/config/configerrordetails.h
+++
@@ -1,9 +0,0 @@
-#include <QDialog>
-
-#pragma once
-
-class ConfigErrorDetails : public QDialog
-{
-public:
-    ConfigErrorDetails(QWidget* parent = nullptr);
-};
--- Brosalin/Code/tools/launcher/openwithprogram.h
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2017-2019 Alejandro Sirgo Rica & Contributors
-
-#pragma once
-
-#include <QPixmap>
-
-void showOpenWithMenu(const QPixmap& capture);
