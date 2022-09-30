ad95927730ea2c0dcda193789539643d3d6d1f4c
RADIOFAN <radiofan22@mail.ru>
2022-09-30 19:45:37 +0700

    + тест правильной работы метода конвертации стоимости в букву + тест правильной работы метода конвертации буквы в стоимость

--- #4/TestProject/TestProject/TestProject.cpp
+++ #4/TestProject/TestProject/TestProject.cpp
@@ -54,6 +54,40 @@ TEST_CLASS(RADIOFAN_FOOL_Tester){
 			Assert::IsTrue(true);
 		}
 
+		/// тест правильной работы метода конвертации стоимости в букву
+		TEST_METHOD(Test_CostConvertor__cost2letter__work){
+			for(uint8_t i=15; i != 2; i++){
+				try{
+			        CostConvertor::cost2letter(i);
+                    Assert::IsTrue(false);
+				}catch(...){
+					Assert::IsTrue(true);
+				}
+			}
+			for(uint8_t i=2; i<10; i++){
+				Assert::AreEqual((wchar_t)(L'0'+i), CostConvertor::cost2letter(i));
+			}
+            Assert::AreEqual(L'0', CostConvertor::cost2letter(10));
+            Assert::AreEqual(L'В', CostConvertor::cost2letter(11));
+            Assert::AreEqual(L'Д', CostConvertor::cost2letter(12));
+            Assert::AreEqual(L'К', CostConvertor::cost2letter(13));
+            Assert::AreEqual(L'Т', CostConvertor::cost2letter(14));
+		}
+
+		/// тест правильной работы метода конвертации буквы в стоимость
+		TEST_METHOD(Test_CostConvertor__cost2letter__work){
+			
+			for(wchar_t i=L'2'; i<L'9'; i++){
+				Assert::AreEqual((uint8_t)(i-L'0'), CostConvertor::letter2cost(i));
+			}
+            Assert::AreEqual((uint8_t)10, CostConvertor::letter2cost(L'0'));
+            Assert::AreEqual((uint8_t)11, CostConvertor::letter2cost(L'В'));
+            Assert::AreEqual((uint8_t)12, CostConvertor::letter2cost(L'Д'));
+            Assert::AreEqual((uint8_t)13, CostConvertor::letter2cost(L'К'));
+            Assert::AreEqual((uint8_t)14, CostConvertor::letter2cost(L'Т'));
+		}
+
+
 
 
 
