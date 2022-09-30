f29dcba536b28d4bece088744dd2e5b13c85be2a
RADIOFAN <radiofan22@mail.ru>
2022-09-30 18:38:57 +0700

    + Тест на проверку реализации синглтона класса приложения

--- #4/TestProject/TestProject/TestProject.cpp
+++ #4/TestProject/TestProject/TestProject.cpp
@@ -14,9 +14,10 @@ namespace TestProject{
 			TEST_METHOD_CLEANUP(END_METHOD){
 
 			}
-		
-			TEST_METHOD(TestMethod1){
-
+			
+			/// тест на проверку реализации синглтона класса приложения
+			TEST_METHOD(Test_App__get_instance){
+				App& game = App::get_instance();
 			}
 	};
 }
