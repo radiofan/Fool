aec1e6ccbf9e49fe8f22abce8a7fb902a44a29ee
RADIOFAN <radiofan22@mail.ru>
2022-09-30 19:31:20 +0700

    * реализация метода конвертации стоимости в букву * реализация метода конвертации буквы в стоимость

--- #4/TestProject/RADIOFAN_FOOL/CostConvertor.h
+++ #4/TestProject/RADIOFAN_FOOL/CostConvertor.h
@@ -1,5 +1,15 @@
 #pragma once
 
 class CostConvertor{
+    public:
+        /// Конвертирует стоимость карты в букву
+        static wchar_t cost2letter(uint8_t cost){
+            return L'\0';
+        }
+
+        /// Конвертирует букву в стоимость карты
+        static uint8_t letter2cost(wchar_t letter){
+            return 0;
+        }
 
 };
