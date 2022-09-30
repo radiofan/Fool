23913ac7a68e7e382f47b8202e94802d14b5ffd4
RADIOFAN <radiofan22@mail.ru>
2022-10-01 00:27:23 +0700

    * реализация метода добавления пар карт в биту класса битых карт

--- #4/TestProject/RADIOFAN_FOOL/BrokenCards.h
+++ #4/TestProject/RADIOFAN_FOOL/BrokenCards.h
@@ -1,6 +1,35 @@
 #pragma once
 
-class BrokenCards{
+class BrokenCards_defenition{
+	private:
+		std::deque<Card*> broken_cards;
+
+
+	protected:
+		BrokenCards_defenition(){
+			reset();
+		}
+
+	public:
+
+		void reset(){
+			broken_cards.clear();
+		}
+
+		uint8_t count(){
+			return broken_cards.size();
+		}
+
+		void add(CardCouple& card_couple){
+			if(card_couple.get_defense())
+				broken_cards.push_front(card_couple.get_defense());
+			if(card_couple.get_attack())
+				broken_cards.push_front(card_couple.get_attack());
+		}
+};
+
+
+class BrokenCards : public BrokenCards_defenition{
 	private:
 		BrokenCards(){}
 		BrokenCards(const BrokenCards&){}  
